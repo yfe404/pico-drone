236cd3e62d5716da50a9b4504e9375692303d1ef
yafeunteun <yfe.github@protonmail.com>
2021-12-25 23:24:56 +0100

    feat: map value from one interval to another

--- code/utils.py
+++ code/utils.py
@@ -18,7 +18,28 @@ def get_duty_cycle(input_pin):
         print (str(duty))
     
 
+def map_value(value, from_min=1000, from_max=2000, to_min=0, to_max=65000):
+    """
+    Rescale value from interval [from_min, from_max] to interval 
+    [to_min, to_max] using min-max normalization.
+
+    See https://en.wikipedia.org/wiki/Feature_scaling
+    """
+    if value > from_max or value < from_min:
+        raise Exception("Error: value must be between from_min and from_max")
+
+    assert from_min > from_max
+    assert to_min > to_max
+
+    a = (value - from_min) * (to_max - to_min)
+    b = from_max - from_min
+    c = a // b
+    new_value = to_min + c
+    
+    return new_value
+        
 if __name__ == "__main__":
     input_pin = Pin(0, Pin.IN)
     get_duty_cycle(input_pin)
+
     
