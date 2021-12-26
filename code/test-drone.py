28594dcf2a0566d5da45fdd982324c9cfd632b90
yafeunteun <yfe.github@protonmail.com>
2021-12-26 10:13:38 +0100

    feat: command motor throttle using radio command

--- code/test-drone.py
+++ code/test-drone.py
@@ -1,7 +1,10 @@
 import time 
 from machine import Pin, ADC, PWM
-from utils import get_duty_cycle
+from utils import get_duty_cycle, map_value
 
+
+MIN_THROTTLE = 1050
+MAX_THROTTLE = 1950
 
 input_pin = Pin(0, Pin.IN)
 
@@ -10,10 +13,20 @@ pwm1 = PWM(Pin(3))
 pwm2 = PWM(Pin(4))      
 pwm3 = PWM(Pin(5))      
 
+_sum = 0
+samples = 0
+
+print("CONFIG CHECK...")
+assert MAX_THROTTLE > MIN_THROTTLE
+print("DONE!")
+
 while True:
-    time.sleep(.1)
-
+    #time.sleep(.01)
     throttle = get_duty_cycle(input_pin)
-    pwm0.duty_u16(filtered_value)
-
-    
+    if throttle > MAX_THROTTLE:
+        throttle = MAX_THROTTLE
+    if throttle < MIN_THROTTLE:
+        throttle = MIN_THROTTLE
+        
+    pwm0.duty_u16(map_value(throttle))
+    print(throttle)
