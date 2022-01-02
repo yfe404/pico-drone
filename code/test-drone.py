132df85f97b558bb613eba7f309727c5cc69d6e0
yafeunteun <yfe.github@protonmail.com>
2022-01-02 22:21:50 +0100

    send throttle to all motors

--- code/test-drone.py
+++ code/test-drone.py
@@ -3,7 +3,7 @@ from machine import Pin, ADC, PWM
 from utils import get_duty_cycle, map_value
 
 
-MIN_THROTTLE = 1050
+MIN_THROTTLE = 1000
 MAX_THROTTLE = 1950
 
 input_pin = Pin(0, Pin.IN)
@@ -18,12 +18,17 @@ assert MAX_THROTTLE > MIN_THROTTLE
 print("DONE!")
 
 while True:
-    #time.sleep(.01)
+    #time.sleep(.2)
+
     throttle = get_duty_cycle(input_pin)
+    throttle -= 30 #offset
+
     if throttle > MAX_THROTTLE:
         throttle = MAX_THROTTLE
     if throttle < MIN_THROTTLE:
         throttle = MIN_THROTTLE
-        
     pwm0.duty_u16(map_value(throttle))
+    pwm1.duty_u16(map_value(throttle))
+    pwm2.duty_u16(map_value(throttle))
+    pwm3.duty_u16(map_value(throttle))
     print(throttle)
